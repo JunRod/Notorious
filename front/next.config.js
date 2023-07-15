8fad84d54c3067d2256240639b24bb106a7d0b49
junior.masna <juniormen.12321@gmail.com>
2023-07-15 18:44:19 -0500

    prueba

--- front/next.config.js
+++ front/next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        dangerouslyAllowSVG: true,
-        contentDispositionType: 'attachment',
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-        minimumCacheTTL: 3600,
-    },
-    http: {
-        server: {
-            http: true,
-        },
-    },
 }
 
 module.exports = nextConfig
