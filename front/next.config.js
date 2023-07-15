57b32385e886a35a4ab950f8d67c597f6e139e9a
junior.masna <juniormen.12321@gmail.com>
2023-07-15 12:00:23 -0500

    build

--- front/next.config.js
+++ front/next.config.js
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    minimumCacheTTL: 3600,
-  },
-  async headers () {
-    return [
-      {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=0; includeSubDomains; preload'
-      }
-    ]
-  }
+    images: {
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        minimumCacheTTL: 3600,
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=0; includeSubDomains; preload'
+                    }
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
