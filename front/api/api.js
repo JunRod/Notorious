a9e6fe6bf988be93d5140b8c46d2b5ff33e536d2
junior.masna <juniormen.12321@gmail.com>
2023-07-16 08:58:55 -0500

    n

--- front/api/api.js
+++ front/api/api.js
@@ -1,18 +1,19 @@
-"use server"
+import {cache} from "react"
+
 //Lear a un .env? De tal manera que importe solo la variable
 const BASE_URL = "https://notoriousback.ddns.net"
 
-export async function getWordsSimilar(wordEnglish) {
+export const getWordsSimilar = cache( async (wordEnglish) => {
     const response = await fetch(`${BASE_URL}/words?word=${wordEnglish}`)
     return response.json()
-}
+})
 
-export async function getIdea(word, wordUse) {
+export const getIdea = cache(async (word, wordUse) => {
     const res = await fetch(`${BASE_URL}/idea?wordOne=${word}&wordTwo=${wordUse}`)
     return res.json()
-}
+})
 
-export async function getImage(history) {
+export const getImage = cache(async(history) => {
     const response = await fetch(`${BASE_URL}/image?history=${history}`)
     return response.json()
-}
+})
