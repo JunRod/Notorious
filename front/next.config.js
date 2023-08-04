c4eab3fe13baaf75007310b04ce165d5082087a0
JuNRod <juniormen.12321@gmail.com>
2023-08-04 13:15:49 -0500

    n

--- front/next.config.js
+++ front/next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXTAUTH_SECRET: 'XEBjSD29ty+26RuBgDnuai7pknoJDrBdLy5lR8b0ROQ=',
-        GOOGLE_CLIENT_ID: '963909375497-p489h0cornaaubm5ckuqfhom5hir1ubi.apps.googleusercontent.com',
-        GOOGLE_CLIENT_SECRET: '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',
-        NEXTAUTH_URL: 'https://notorious-799tu.ondigitalocean.app'
-    }
 }
 
 module.exports = nextConfig
